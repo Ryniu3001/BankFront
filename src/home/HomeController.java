cca42984bd175e55d35946f7c9e86d494374c8cb
Ryniu <ryniu3001@gmail.com>
2016-11-29 22:11:41 +0100

    Initial commit

---
+++ src/home/HomeController.java
@@ -0,0 +1,16 @@
+package home;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by marcin on 29.11.16.
+ */
+public class HomeController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
